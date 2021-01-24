c143bb7e85fa0e145d7b3650a4552da4ea9160b0
Don-Stevenson <dsteven1@gmail.com>
2021-01-23 23:48:56 -0500

    working add member and get member list

--- src/App.js
+++ src/App.js
@@ -1,81 +1,50 @@
-import React, { useState, useContext } from "react";
-// import { useHistory } from "react-router-dom";
-// import UserContext from "../../context/UserContext";
-// import ErrorNotice from "../misc/ErrorNotice"
-// import Axios from "axios";
-
+import React from "react";
 import "./App.css";
+import fetch from "node-fetch";
 
-const apiKey = process.env.API_KEY;
-const planKey = process.env.PLAN_KEY;
+const apiKey = process.env.REACT_APP_API;
+const planKey = process.env.REACT_APP_PLAN;
 
 console.log({ planKey }, { apiKey });
 
 function App() {
-  // const addMember = async () => {
-  //   const memberPost = await fetch(
-  //     "https://cors-anywhere.herokuapp.com/https://api.memberstack.com/v1/members",
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         "X-Api-Key": "57e5e61efabdf3aec64c7f0f6ecfd1e4",
-  //       },
-  //       body: JSON.stringify({
-  //         email: "vka000ttrrrtt0@gmail.com",
-  //         password: "123456789",
-  //         plan: "5ffe38ec8e513c0004bd14e5",
-  //       }),
-  //     }
-  //   );
-  //   console.log({ memberPost });
-  // };
-
+  const addMember = (email, password) => {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.memberstack.com/v1/members",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": apiKey,
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          plan: planKey,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log("POST DATA: ", data))
+      .catch((e) => console.log(e));
+  };
 
   // Working'S GET
-  // const memberList = await fetch(
-  //   "https://cors-anywhere.herokuapp.com/https://api.memberstack.com/v1/members",
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "X-Api-Key": "57e5e61efabdf3aec64c7f0f6ecfd1e4",
-  //     },
-  // body: JSON.stringify({
-  //   email: "vka000ttrrrtt0@gmail.com",
-  //   password: "123456789",
-  //   plan: "5ffe38ec8e513c0004bd14e5",
-  // })
-  //   }
-  // );
+  const getMembers = () => {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.memberstack.com/v1/members",
+      {
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data.members))
+      .catch((e) => console.log(e));
+  };
 
-  // console.log({ res });
-  //fetch('https://api.memberstack.com/v1/members', {
-  // headers: {
-  //   'X-API-KEY': 'yourapikey'
-  // }
-
-  // listMembers();
-  // addMember();
-
-  return (
-    <div className="App">
-      In the try memberstack api
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Rdasdsd
-        </a>
-      </header> */}
-    </div>
-  );
+  console.log(getMembers(), addMember())
+  return <div className="App">In the try memberstack api</div>;
 }
 export default App;
